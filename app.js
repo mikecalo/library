2887cf2a22c6192c16f55ed280e814a9bac9f006
Michael Calandriello <michaelcole5950@gmail.com>
2024-10-06 10:18:03 -0500

    UI updates. input form and card placeholder for book entries

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("HI")
+const myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+}
+
+function addBookToLibrary(title, author, pages, isRead) {
+  const newBook = new Book(title, author, pages, isRead);
+  myLibrary.push(newBook);
+}
+
+addBookToLibrary("harry p", "jk", 400, false);
+addBookToLibrary("hibbity dibbity", "huey", 1000, true);
+
+function displayBooks() {
+  myLibrary.forEach((book) => {
+    console.log(book);
+  });
+}
+
+displayBooks();
