f75b57cfe2699bc477dc047bc5fbe391cfb8bd93
Michael Calandriello <michaelcole5950@gmail.com>
2024-10-07 20:48:47 -0500

    implemented part of js - adding & displaying books. TODO: debug issues on card styling, read checkbox func to toggle green banner, delete functionality, local storage functionality.

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const newBookBtn = document.querySelector('#newBookBtn')
+const addBookBtn = document.querySelector('#addBookBtn')
+const deleteBookBtn = document.querySelector('#deleteBookBtm')
+const cardContainer = document.querySelector('#cardContainer')
+
 const myLibrary = [];
 
 function Book(title, author, pages, isRead) {
@@ -7,18 +12,59 @@ function Book(title, author, pages, isRead) {
   this.isRead = isRead;
 }
 
-function addBookToLibrary(title, author, pages, isRead) {
-  const newBook = new Book(title, author, pages, isRead);
-  myLibrary.push(newBook);
+function addBookToLibrary(event) {
+	event.preventDefault();
+	const title = document.querySelector('#title')
+	const author= document.querySelector('#author')
+	const pages = document.querySelector('#pages')
+	const isRead = document.querySelector('#isRead')
+	const newBook = new Book(title.value, author.value, pages.value, isRead.checked);
+  	myLibrary.push(newBook);
+	displayBooks()
 }
 
-addBookToLibrary("harry p", "jk", 400, false);
-addBookToLibrary("hibbity dibbity", "huey", 1000, true);
-
 function displayBooks() {
-  myLibrary.forEach((book) => {
-    console.log(book);
+	clearBooks()
+  	myLibrary.forEach((book) => {
+		const bookHTML = `
+			<div class="card mb-3">
+				<div class="card-body">
+					<h5 class="card-title" id="title">${book.title}</h5>
+					<p class="card-text" id="author">Author: ${book.author}</p>
+					<p class="card-text" id="pages">Pages: ${book.pages}</p>
+					<input class="form-check-input" type="checkbox" id="isReadCard" ${book.isRead ? 'checked' : ''}>
+					<label class="form-check-label" for="isReadCard">
+						Read
+					</label>
+				</div>
+				<button class="btn btn-danger" id="deleteButton">
+				<i class="material-symbols-outlined text-light">delete</i>
+				</button>
+			</div>
+	`;
+		const cardElement = document.createElement('div')
+		cardElement.classList.add('col-sm-3', 'cards')
+		if (book.isRead) {
+			cardElement.classList.add('read')
+		}
+		cardElement.innerHTML = bookHTML
+		cardContainer.appendChild(cardElement)
   });
 }
 
-displayBooks();
+function clearBooks() {
+	cardContainer.innerHTML = ''
+}
+
+// Events
+newBookBtn.addEventListener("click", () => {
+	let formContainer = document.querySelector('#formContainer')
+	if (formContainer.style.display == 'none') {
+		formContainer.style.display = 'block'
+	} else {
+		formContainer.style.display = 'none'
+	}
+})
+
+addBookBtn.addEventListener('click', addBookToLibrary)
+
