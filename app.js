c766af8aa9525f1b6e4b6cc7ebc953c5c55a99dd
Michael Calandriello <michaelcole5950@gmail.com>
2024-10-08 20:49:34 -0500

    addressed TODOs: read and delete functionality. still TODO: local storage.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const newBookBtn = document.querySelector('#newBookBtn')
 const addBookBtn = document.querySelector('#addBookBtn')
-const deleteBookBtn = document.querySelector('#deleteBookBtm')
 const cardContainer = document.querySelector('#cardContainer')
 
 const myLibrary = [];
@@ -10,6 +9,10 @@ function Book(title, author, pages, isRead) {
   this.author = author;
   this.pages = pages;
   this.isRead = isRead;
+}
+
+Book.prototype.toggleRead = function() {
+	this.isRead = !this.isRead
 }
 
 function addBookToLibrary(event) {
@@ -25,25 +28,23 @@ function addBookToLibrary(event) {
 
 function displayBooks() {
 	clearBooks()
-  	myLibrary.forEach((book) => {
+  	myLibrary.forEach((book, index) => {
 		const bookHTML = `
-			<div class="card mb-3">
-				<div class="card-body">
-					<h5 class="card-title" id="title">${book.title}</h5>
-					<p class="card-text" id="author">Author: ${book.author}</p>
-					<p class="card-text" id="pages">Pages: ${book.pages}</p>
-					<input class="form-check-input" type="checkbox" id="isReadCard" ${book.isRead ? 'checked' : ''}>
-					<label class="form-check-label" for="isReadCard">
-						Read
-					</label>
-				</div>
-				<button class="btn btn-danger" id="deleteButton">
-				<i class="material-symbols-outlined text-light">delete</i>
-				</button>
-			</div>
+		<div class="card-body">
+		<h5 class="card-title" id="title">${book.title}</h5>
+		<p class="card-text" id="author">Author: ${book.author}</p>
+		<p class="card-text" id="pages">Pages: ${book.pages}</p>
+		<input class="form-check-input" type="checkbox" onclick=updateRead(${index}) id="isReadCard" ${book.isRead ? 'checked' : ''}>
+        <label class="form-check-label readStatus" for="isReadCard">
+        Read
+        </label>
+		</div>
+		<button class="btn btn-danger" onclick=deleteBook(${index}) id="deleteBookBtn">
+		<i class="material-symbols-outlined text-light">delete</i>
+		</button>
 	`;
 		const cardElement = document.createElement('div')
-		cardElement.classList.add('col-sm-3', 'cards')
+		cardElement.classList.add('col-sm-3', 'cards', 'card', 'm-1', 'bg-primary', 'text-light')
 		if (book.isRead) {
 			cardElement.classList.add('read')
 		}
@@ -56,7 +57,18 @@ function clearBooks() {
 	cardContainer.innerHTML = ''
 }
 
-// Events
+function deleteBook (index) {
+	myLibrary.splice(index, 1)
+	displayBooks()
+}
+
+function updateRead(index) {
+	myLibrary[index].toggleRead()
+	displayBooks()
+}
+
+// EVENTS
+// toggle form
 newBookBtn.addEventListener("click", () => {
 	let formContainer = document.querySelector('#formContainer')
 	if (formContainer.style.display == 'none') {
@@ -66,5 +78,7 @@ newBookBtn.addEventListener("click", () => {
 	}
 })
 
+// add book entered by user via form
 addBookBtn.addEventListener('click', addBookToLibrary)
+
 
